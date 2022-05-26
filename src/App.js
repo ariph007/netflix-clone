870cafee1ebb2429a6a4e5e3119d6eea7edf4207
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-05-26 15:20:48 +0700

    Added Sign In and Sign Up Page, and Firebase Config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx'
+import { AuthContextProvide } from './context/AuthContext.js';
 import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Account from './pages/Account.jsx'
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvide>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </AuthContextProvide>
     </>
   );
 }
